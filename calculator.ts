c865a6ba16cd061fc5659f86b9f143977badc785
saima-salar <157168170+saima-salar@users.noreply.github.com>
2024-04-03 02:43:29 +0500

    Add files via upload
    Governor Sindh IT  Initiative Program (online Class-2 for onsite students)

---
+++ calculator.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "ENTER second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "Multiplication", "division"],
+  },
+]);
+// conditional statment
+// console.log("your value is:" + answer.firstNumber+answer.secondNumber);answer 1020(due to data type string)
+
+if (answer.operator === "addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+}
+else{
+    console.log("please select valid operator");
+}
